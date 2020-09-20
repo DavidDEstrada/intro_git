9b2a74408b459d706ced4c87ce4a7f0ffecbe5b3
David Estrada <david.estrada2472@alumnos.udg.mx>
2020-09-20 11:55:39 -0500

    Aun funciona el hola mundo

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main () {
+    char nombre[100];
 
-    printf("Hola Mundo");
-
+    printf("Hola Mundo\n");
+    printf("Nombre: ");
+    scanf("%s", nombre);
+    printf("Hola %s\n", nombre);
     return 0;
 }
