695265f75a2f48d6d41f513ae6bd941285edd72b
David Estrada <david.estrada2472@alumnos.udg.mx>
2020-09-20 12:28:38 -0500

    Capturamos edad e imprimimos la edad mas 1

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 int main () {
     char nombre[100];
-
+	unsigned int edad;
     printf("Hola Mundo\n");
     printf("Nombre: ");
-    scanf("%s", nombre);
+    fgets(nombre, sizeof(nombre), stdin);
     printf("Hola buenas %s\n", nombre);
+    printf("Edad: ");
+    scanf("%u", &edad);
+    printf("Vas a cumplir %u\n", edad +1);
     return 0;
 }
